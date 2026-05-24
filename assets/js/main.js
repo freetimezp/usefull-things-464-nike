4dcca6478df00e2682b2cfa841f57e8b432cd2fc
Yevhen Onoprienko <fromzpcity@gmail.com>
2026-05-24 17:59:54 +0300

    02. usefull-things-464-nike. Header.

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+
+if (navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add("show-menu");
+    });
+}
+
+if (navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("show-menu");
+    });
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+    const navMenu = document.getElementById("nav-menu");
+
+    navMenu.classList.remove("show-menu");
+};
+
+navLink.forEach((el) => el.addEventListener("click", linkAction));
+
+/*=============== CHANGE HEADER STYLES ===============*/
+
+/*=============== SWIPER JS ===============*/
+
+/*=============== GSAP ANIMATION ===============*/
+
+/*=============== BUTTON MOUSE MOVE ===============*/
